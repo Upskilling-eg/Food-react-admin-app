b00643e423489748feb86759b9732cb7c5af9139
Upskilling-eg <you@example.com>
2023-11-25 01:49:20 +0200

    session5

--- src/CategoriesModule/Components/CategoriesList/CategoriesList.jsx
+++ src/CategoriesModule/Components/CategoriesList/CategoriesList.jsx
@@ -4,11 +4,14 @@ import axios from "axios";
 import NoData from "../../../SharedModule/Components/NoData/NoData";
 import Modal from "react-bootstrap/Modal";
 import { useForm } from "react-hook-form";
+import noData from "../../../assets/images/no-data.png";
+import { ToastContainer, toast } from "react-toastify";
 
 export default function CategoriesList() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors },
   } = useForm();
   const onSubmit = (data) => {
@@ -19,7 +22,16 @@ export default function CategoriesList() {
         },
       })
       .then((response) => {
-        ///////////////////////////////
+        toast.success("added successsfully", {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
         handleClose();
         getCategoriesList();
       })
@@ -28,15 +40,77 @@ export default function CategoriesList() {
       });
   };
   const [categoriesList, setCategoriesList] = useState([]);
-  const [show, setShow] = useState(false);
+  const [modalState, setModalState] = useState("close");
+  const [itemId, setItemId] = useState(0);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const showAddModal = () => {
+    setValue("name", null);
+    setModalState("modal-one");
+  };
 
+  const showDeleteModal = (id) => {
+    setItemId(id);
+    setModalState("modal-two");
+  };
+
+  const showUpdateModal = (categoryItem) => {
+    setItemId(categoryItem.id);
+    setValue("name", categoryItem.name);
+    setModalState("modal-three");
+  };
+
+  const handleClose = () => setModalState("close");
+  const deleteCategory = () => {
+    axios
+      .delete(`http://upskilling-egypt.com:3002/api/v1/Category/${itemId}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("adminToken")}`,
+        },
+      })
+      .then((response) => {
+        toast.success("delete successsfully", {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+        handleClose();
+        getCategoriesList();
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const updateCategory = (data) => {
+    axios
+      .put(`http://upskilling-egypt.com:3002/api/v1/Category/${itemId}`, data, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("adminToken")}`,
+        },
+      })
+      .then((response) => {
+        toast.success("update successsfully", {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+        handleClose();
+        getCategoriesList();
+      })
+      .catch((error) => console.log(error));
+  };
   const getCategoriesList = () => {
     axios
       .get(
-        "http://upskilling-egypt.com:3002/api/v1/Category/?pageSize=10&pageNumber=1",
+        "http://upskilling-egypt.com:3002/api/v1/Category/?pageSize=20&pageNumber=1",
         {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("adminToken")}`,
@@ -56,13 +130,15 @@ export default function CategoriesList() {
 
   return (
     <>
+      <ToastContainer />
+
       <Header
         title={"Categories Items"}
         paragraph={
           "You can now add your items that any user can order it from the Application and you can edit"
         }
       />
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={modalState === "modal-one"} onHide={handleClose}>
         <Modal.Body>
           <h4>add new category</h4>
           <form onSubmit={handleSubmit(onSubmit)}>
@@ -83,6 +159,50 @@ export default function CategoriesList() {
           </form>
         </Modal.Body>
       </Modal>
+
+      <Modal show={modalState === "modal-two"} onHide={handleClose}>
+        <Modal.Body>
+          <div className="text-center">
+            <img src={noData} alt="" />
+            <h5 className="my-2">Delete This Item ?</h5>
+            <span className="text-muted">
+              are you sure you want to delete this item ? if you are sure just
+              click on delete it
+            </span>
+            <div className="text-end my-3">
+              <button
+                onClick={deleteCategory}
+                className="btn btn-outline-danger"
+              >
+                Delete this item
+              </button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+
+      <Modal show={modalState === "modal-three"} onHide={handleClose}>
+        <Modal.Body>
+          <h4>update new category</h4>
+          <form onSubmit={handleSubmit(updateCategory)}>
+            <div className="form-group my-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="please enter category name"
+                {...register("name", { required: true })}
+              />
+              {errors.name && errors.name.type === "required" && (
+                <span className="text-danger my-2">field is required</span>
+              )}
+            </div>
+            <div className="form-group">
+              <button className="btn btn-success w-100">Update Category</button>
+            </div>
+          </form>
+        </Modal.Body>
+      </Modal>
+
       <div className="row  mx-4 p-3">
         <div className="col-md-6">
           <div>
@@ -92,7 +212,7 @@ export default function CategoriesList() {
         </div>
         <div className="col-md-6">
           <div className="text-end">
-            <button onClick={handleShow} className="btn btn-success">
+            <button onClick={showAddModal} className="btn btn-success">
               Add New Category
             </button>
           </div>
@@ -113,7 +233,16 @@ export default function CategoriesList() {
                   <tr key={category.id}>
                     <th scope="row">{category.id}</th>
                     <td>{category.name}</td>
-                    <td></td>
+                    <td>
+                      <i
+                        onClick={() => showUpdateModal(category)}
+                        className="fa fa-edit text-warning fa-2x mx-2"
+                      ></i>
+                      <i
+                        onClick={() => showDeleteModal(category.id)}
+                        className="fa fa-trash text-danger fa-2x"
+                      ></i>
+                    </td>
                   </tr>
                 ))}
               </tbody>
--- src/RecipesModule/Components/RecipesList/RecipesList.jsx
+++ src/RecipesModule/Components/RecipesList/RecipesList.jsx
@@ -1,14 +1,170 @@
-import React from 'react'
-import Header from '../../../SharedModule/Components/Header/Header';
+import React, { useEffect, useState } from "react";
+import Header from "../../../SharedModule/Components/Header/Header";
+import NoData from "../../../SharedModule/Components/NoData/NoData";
+import axios from "axios";
+import noData from "../../../assets/images/no-data.png";
+import Modal from "react-bootstrap/Modal";
+import { toast, ToastContainer } from "react-toastify";
 
 export default function RecipesList() {
+  const [RecipesList, setRecipesList] = useState([]);
+  const [modalState, setModalState] = useState("close");
+  const [itemId, setItemId] = useState(0);
+  const handleClose = () => setModalState("close");
+
+  const showDeleteModal = (id) => {
+    setItemId(id);
+    setModalState("delete-modal");
+  };
+
+  const deleteRecipe = () => {
+    axios
+      .delete(`http://upskilling-egypt.com:3002/api/v1/Recipe/${itemId}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("adminToken")}`,
+        },
+      })
+      .then((response) => {
+        toast.success("delete successsfully", {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+        handleClose();
+        getAllRecipes();
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const getAllRecipes = () => {
+    axios
+      .get(
+        "http://upskilling-egypt.com:3002/api/v1/Recipe/?pageSize=10&pageNumber=1",
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("adminToken")}`,
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+        setRecipesList(response?.data?.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getAllRecipes();
+  }, []);
   return (
     <>
-         <Header title={'welcome recipeeees'} paragraph={'recipeeees parapohrah'}/>
+      <ToastContainer />
+      <Modal show={modalState === "delete-modal"} onHide={handleClose}>
+        <Modal.Body>
+          <div className="text-center">
+            <img src={noData} alt="" />
+            <h5 className="my-2">Delete This Item ?</h5>
+            <span className="text-muted">
+              are you sure you want to delete this item ? if you are sure just
+              click on delete it
+            </span>
+            <div className="text-end my-3">
+              <button onClick={deleteRecipe} className="btn btn-outline-danger">
+                Delete this item
+              </button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+      <Header
+        title={"Recipes Items"}
+        paragraph={
+          "You can now add your items that any user can order it from the Application and you can edit"
+        }
+      />
 
-      <div>RecipesList</div>
+      <div className="row  mx-4 p-3">
+        <div className="col-md-6">
+          <div>
+            <h6>Recipes Table Details</h6>
+            <span>You can check all details</span>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="text-end">
+            <button className="btn btn-success">Add New Recipe</button>
+          </div>
+        </div>
 
+        <div>
+          {RecipesList.length > 0 ? (
+            <table className="table my-5">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Recipe Name</th>
+                  <th scope="col">Image</th>
+                  <th scope="col">Price</th>
+                  <th scope="col">Description</th>
+                  <th scope="col">Category</th>
+                  <th scope="col">Tag</th>
+                  <th scope="col">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {RecipesList.map((recipe) => (
+                  <tr key={recipe.id}>
+                    <th scope="row">{recipe.id}</th>
+                    <td>{recipe.name}</td>
+
+                    <td>
+                      <div className="img-container">
+                        {recipe.imagePath ? (
+                          <img
+                            className="img-fluid"
+                            src={
+                              `http://upskilling-egypt.com:3002/` +
+                              recipe.imagePath
+                            }
+                            alt=""
+                          />
+                        ) : (
+                          <img className="img-fluid" src={noData} alt="" />
+                        )}
+                      </div>
+                    </td>
+
+                    <td>{recipe.price}</td>
+                    <td>{recipe.description}</td>
+                    <td>{recipe.category[0]?.name}</td>
+                    <td>{recipe.tag.name}</td>
+
+                    <td>
+                      {/* <i
+                        onClick={() => showUpdateModal(category)}
+                        className="fa fa-edit text-warning fa-2x mx-2"
+                      ></i> */}
+                      <i
+                        onClick={() => showDeleteModal(recipe.id)}
+                        className="fa fa-trash text-danger fa-2x"
+                      ></i>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <NoData />
+          )}
+        </div>
+      </div>
     </>
-
-  )
+  );
 }
